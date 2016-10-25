fd038a33ca0b33ac3864e96243f19fc980623f30
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2016-10-25 21:15:59 +0200

    fixed wrong formula

--- src/test/java/com/samuelkupferschmid/fhnw/dist/DataTrainingTest.java
+++ src/test/java/com/samuelkupferschmid/fhnw/dist/DataTrainingTest.java
@@ -34,7 +34,7 @@ public class DataTrainingTest {
 
         Performance perf = trainer.train(spamTest,hamTest,10);
 
-        assertTrue(String.valueOf("f1 score after training: " +perf.getF1Score()), perf.getF1Score() > 0.5);
+        assertTrue(String.valueOf("f1 score after training: " +perf.getF1Score()), perf.getF1Score() > 0.8);
     }
 
     @Test
@@ -42,13 +42,13 @@ public class DataTrainingTest {
 
         Spamfilter spamfilter = new Spamfilter(spamTrain,hamTrain);
         Performance perf = new Trainer(spamfilter,null,null).calculatePerformance(spamTest,hamTest,0.5);
-        assertTrue(String.valueOf(perf.getF1Score()), perf.getF1Score() > 0.5);
+        assertTrue(String.valueOf(perf.getF1Score()), perf.getF1Score() > 0.8);
     }
 
     @Test
     public void TestSpamFilter2() {
         Spamfilter spamfilter = new Spamfilter(new String[]{"Dies ist Spam"},new String[]{"Hier haben wir Ham"});
         Performance perf = new Trainer(spamfilter,null,null).calculatePerformance(new String[]{"Spam is hier"},new String[]{"Ham haben wir auch viel"},0.5);
-        assertTrue(String.valueOf(perf.getF1Score()), perf.getF1Score() > 0.5);
+        assertTrue(String.valueOf(perf.getF1Score()), perf.getF1Score() > 0.8);
     }
 }
